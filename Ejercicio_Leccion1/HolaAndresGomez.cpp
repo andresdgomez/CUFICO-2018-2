66ddd69048783d529e9d303ec8cbfc4e26d05b1e
Velpe <esneider.velezp@udea.edu.co>
2018-08-14 13:55:05 -0500

    ejercicio primera clase FCI

---
+++ Ejercicio_Leccion1/HolaAndresGomez.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+//using namespace std;
+
+int main() {                             //inicio de la funcion main
+	std::cout << "Hola mundo" << std::endl;      //imprimir en pantalla}
+	return 0;
+}
